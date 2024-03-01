c089a718d3780428c3da082357b66bb85b43c82c
simba2211 <118442072+simba2211@users.noreply.github.com>
2024-03-01 15:29:54 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -66,19 +66,19 @@ Current time: {{time}}`;
 export const DEFAULT_MODELS = [
   {
     name: "gpt-4",
-    available: true,
+    available: false,
   },
   {
     name: "gpt-4-32k",
-    available: true,
+    available: false,
   },
   {
     name: "gpt-4-1106-preview",
-    available: true,
+    available: false,
   },
   {
     name: "gpt-4-vision-preview",
-    available: true,
+    available: false,
   },
   {
     name: "gpt-3.5-turbo",
